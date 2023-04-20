bebf85effd196ecbdd3cdfc16b7760921f39f38c
Jesus-0sorio <jaoz10.16@gmail.com>
2023-04-20 15:16:41 -0500

    ruta settings

--- src/Components/Profile/ProfileBar.jsx
+++ src/Components/Profile/ProfileBar.jsx
@@ -16,7 +16,7 @@ export const ProfileBar = () => {
 						alt='Profile'
 						className='w-36 h-36 rounded-full'
 					/>
-					<NavLink to="/settings" className='bg-blue-500 hover:bg-blue-600 text-white rounded-full px-4 py-2 text-sm font-semibold mt-2'>
+					<NavLink to="/storyCloud/settings" className='bg-blue-500 hover:bg-blue-600 text-white rounded-full px-4 py-2 text-sm font-semibold mt-2'>
 						Editar perfil
 					</NavLink>
 				</div>
