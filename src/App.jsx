9383dc1289d417943f78be5b00f3ef604b0a98df
Jesus-0sorio <jaoz10.16@gmail.com>
2023-04-20 14:56:15 -0500

    change rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,64 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ProtectedRoute } from './Components/ProtectedRoute';
-import { Home, Login, SignUp, PageNoFound, Profile, Settings, Messages } from './Pages';
-import { AuthContext } from './context/AuthContext';
 import { Navbar } from './Components/Shared/Navbar';
-
+import {
+	Home,
+	Login,
+	Messages,
+	PageNoFound,
+	Profile,
+	Settings,
+	SignUp,
+} from './Pages';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<AuthContext>
-						<Navbar />
-						<Routes>
-							<Route
-								path='/login'
-								element={<Login />}
-							/>
-							<Route
-								path='/signup'
-								element={<SignUp />}
-							/>
-							<Route
-								path='/profile/:id'
-								element={
-									<ProtectedRoute>
-										<Profile />
-									</ProtectedRoute>
-								}
-							/>
-							<Route
-								path='/'
-								element={
-									<ProtectedRoute>
-										<Home />
-									</ProtectedRoute>
-								}
-							/>
-							<Route
-								path='/settings'
-								element={
-									<ProtectedRoute>
-										<Settings />
-									</ProtectedRoute>
-								}
-							/>
-							<Route
-								path='/messages'
-								element={
-										<Messages />
-								}
-							/>
-							<Route
-								path='*'
-								element={<PageNoFound />}
-							/>
-						</Routes>
+				<Navbar />
+				<Routes>
+					<Route
+						path='/storyCloud/login'
+						element={<Login />}
+					/>
+					<Route
+						path='/storyCloud/signup'
+						element={<SignUp />}
+					/>
+					<Route
+						path='/storyCloud/profile/:id'
+						element={
+							<ProtectedRoute>
+								<Profile />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/storyCloud/'
+						element={
+							<ProtectedRoute>
+								<Home />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/storyCloud/settings'
+						element={
+							<ProtectedRoute>
+								<Settings />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path='/storyCloud/messages'
+						element={<Messages />}
+					/>
+					<Route
+						path='*'
+						element={<PageNoFound />}
+					/>
+				</Routes>
 			</AuthContext>
 		</BrowserRouter>
 	);
